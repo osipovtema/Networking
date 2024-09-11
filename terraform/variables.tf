fac7725f42da0a95396dbddf4db343abdbb2a25f
osipovtema <33830995+osipovtema@users.noreply.github.com>
2024-09-11 19:55:13 +0300

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "yandex_cloud_id" {
+  default = "b1g4m8qmhdgh8k9b16jr"
+}
+
+variable "yandex_folder_id" {
+  default = "b1g72hofn0r6eom3l3oo"
+}
+
+variable "a-zone" {
+  default = "ru-central1-c"
+}
+
+variable "nat-instance-image-id" {
+  default = "fd80mrhj8fl2oe87o4e1"
+}
+
+variable "nat-instance-ip" {
+  default = "192.168.10.254"
+}
+
+variable "centos-7-base" {
+  default = "fd88mqi0d02c7jbkest0"
+}
+
+variable "domain" {
+  default = "netology.cloud"
+}
+
+variable "nat-instance-name" {
+  default = "nat-instance-vm1"
+}
+
+variable "public-vm-name" {
+  default = "public-vm1"
+}
+
+variable "private-vm-name" {
+  default = "private-vm1"
+}
